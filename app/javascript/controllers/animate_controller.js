8420df550521b26de35483e13c81abce682657f2
Miguel Dagatan <miguel.dagatan@gmail.com>
2024-11-10 02:13:38 +0800

    Add initial design

--- app/javascript/controllers/animate_controller.js
+++ app/javascript/controllers/animate_controller.js
@@ -3,14 +3,35 @@ import { gsap } from "gsap"
 
 export default class extends Controller {
   connect() {
-    console.log("Hello, Stimulus!", this.element)
-    const timeline = gsap.timeline()
+    const timeline = gsap.timeline({
+      defaults: {
+        duration: 1,
+        ease: "power1.inOut"
+      },
+      onComplete: () => {
+        // Remove the hidden class after the first timeline completes
+        const slide1 = document.getElementById("our_story")
+        slide1.classList.remove("hidden")
+
+        slide1.scrollIntoView(  { behavior: 'smooth', block: 'start' })
+    
+        // Start the second timeline
+        timeline2.play()
+      }
+    })
 
     // Define each step in the animation
     timeline
-      .to(this.element, { opacity: 1, duration: 0.5 })
-      .to(this.element, { x: 100, duration: 0.5 })
-      .to(this.element, { y: 100, duration: 0.5 })
-      .to(this.element, { rotation: 360, duration: 0.5 })
+      .to("#loading", { opacity: 1, duration: 3, y: 0 })
+      .to("#loading-spinner", { opacity: 1, duration: 1})
+      .to("#loading-spinner", { opacity: 0, duration: 1, delay: 1})
+
+    const timeline2 = gsap.timeline({ paused: true })
+
+    timeline2
+      .to("#ella1", { opacity: 1, x: 0, duration: 0.3, ease: "sine.inOut" })
+
+      
+     
   }
 }
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,8 +12,17 @@ module.exports = {
       fontFamily: {
         main: ['Crimson Pro', ...defaultTheme.fontFamily.sans],
         header: ['Playlist Script', ...defaultTheme.fontFamily.sans],
+        logo: ['Bellefair', ...defaultTheme.fontFamily.sans],
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        pastelPink: '#f8dbe0',
+        pink: '#f2a9b6',
+        darkPink: '#ed8092',
+        pastelBlue: '#bed5e7',
+        pastelYellow: '#f4de95',
+        pastelOrange: '#f6bd7b'
+      }
     },
   },
   plugins: [
